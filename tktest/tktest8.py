7086bbcc618121a0bd63138a393160924da6dfd7
Joshua Brown <jg95brown@gmail.com>
2018-03-05 21:07:43 -0600

    Create tktest8.py

---
+++ tktest/tktest8.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+class BuckysButtons:
+def __init__(self, master):
+frame = tk.Frame(master)
+frame.pack()
+
+self.printButton = tk.Button
+		
+
+
+
+root = tk.Tk()
+root.mainloop()
