640246a732eae742421e3e63588c81e160f2cd09
Joshua Brown <jg95brown@gmail.com>
2018-03-07 16:39:12 -0600

    tktest13

--- tktest/tktest13.py
+++ tktest/tktest13.py
@@ -3,4 +3,11 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry("500x300+60+50")
 
+canvas = tk.Canvas(root, width=200, height=100)
+canvas.pack()
+
+
+blackline = canvas.create_line(0,0, 200,50)
+redline = canvas.create_line(0,100, 200,50, fill="red")
+
 root.mainloop()
